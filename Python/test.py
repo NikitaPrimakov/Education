f91e7f1c54fec7c27805c6f660e9d21515ab5630
PrimakovNA <97438140+NikitaPrimakov@users.noreply.github.com>
2025-04-14 15:42:29 +0500

    add 20-day

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,11 @@
-# Символы всех строк
+# Без дубликатов
 
-# На вход программе подаются натуральное число n, а затем n строк. Напишите программу, которая создает 
-# список из символов всех строк, а затем выводит его.
+# На вход программе подаются натуральное число n, а затем n строк. Напишите программу, которая выводит 
+# только уникальные строки, в том же порядке, в котором они были введены.
 
-n = int(input("Enter number: "))
+n = int(input("Enter your number: "))
+
+# str_one = input("Enter your string: ")
 
 list_one = list()
 
@@ -11,6 +13,8 @@ for i in range(n):
 
     str_one = input("Enter your string: ")
 
-    list_one.extend(str_one)
+    if str_one not in list_one:
+        
+        list_one.append(str_one)
 
-print(list_one)
+print(*list_one, sep='\n')
