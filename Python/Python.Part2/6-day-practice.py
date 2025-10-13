1b4ad47ddde638c711a7d4ec9c04fdf35d814ee2
NikitaPrimakov <nikita.primakov.98@gmail.com>
2025-10-13 14:27:25 +0300

    add code

--- Python/Python.Part2/6-day-practice.py
+++ Python/Python.Part2/6-day-practice.py
@@ -226,3 +226,45 @@ for i in range(num):
             if matrix[i][j] > max_num:
                 max_num = matrix[i][j]
 print(max_num)
+
+#------------------------------------
+# Задача 6
+# Суммы четвертей
+
+# Квадратная матрица разбивается на четыре четверти, ограниченные главной и побочной диагоналями: верхнюю, нижнюю, левую и правую.
+
+
+num = int(input('Enter size of matrix: '))
+
+matrix = []
+
+sum_1 = 0
+sum_2 = 0
+sum_3 = 0
+sum_4 = 0
+
+
+for i in range(num):
+
+    row = input().split(' ')
+    for j in range(len(row)):
+        row[j] = int(row[j])
+    matrix.append(row)
+
+
+for i in range(num):
+    for j in range(num):
+        if i < j and i < num - 1 - j:
+            sum_1 += matrix[i][j]
+        if i < j and i > num - 1 - j:
+            sum_2 += matrix[i][j]
+        if i > j and i > num - 1 -j:
+            sum_3 += matrix[i][j]
+        if i > j and i < num - 1 - j:
+            sum_4 += matrix[i][j]
+
+
+print('Верхняя четверть:',sum_1)
+print('Правая четверть:', sum_2)
+print('Нижняя четверть:', sum_3)
+print('Левая четверть:', sum_4)
--- Python/Python.Part2/test.py
+++ Python/Python.Part2/test.py
@@ -10,6 +10,9 @@ num = int(input('Enter size of matrix: '))
 matrix = []
 
 sum_1 = 0
+sum_2 = 0
+sum_3 = 0
+sum_4 = 0
 
 
 for i in range(num):
@@ -19,15 +22,20 @@ for i in range(num):
         row[j] = int(row[j])
     matrix.append(row)
 
+
 for i in range(num):
     for j in range(num):
-
-        if i > j and i < num - 1 - j:
-            print('i > j and i < num - 1 -j:', matrix[i][j])
-            sum_1 += matrix[i][j]
         if i < j and i < num - 1 - j:
-            print('i < j and i < num - 1 - j:', matrix[i][j])
+            sum_1 += matrix[i][j]
         if i < j and i > num - 1 - j:
-            print('i < j and i > num - 1 - j:', matrix[i][j])
+            sum_2 += matrix[i][j]
         if i > j and i > num - 1 -j:
-            print('i > j and i > num - 1 -j:', matrix[i][j])
+            sum_3 += matrix[i][j]
+        if i > j and i < num - 1 - j:
+            sum_4 += matrix[i][j]
+
+
+print('Верхняя четверть:',sum_1)
+print('Правая четверть:', sum_2)
+print('Нижняя четверть:', sum_3)
+print('Левая четверть:', sum_4)
